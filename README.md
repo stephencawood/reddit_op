84d19d8d9301c6002975c156c478380b68fe0f7b
cawood <cawood@gmail.com>
2020-02-02 15:56:57 -0800

    added parameter for subreddit

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Reddit Posts Op for CTO.ai Ops CLI
 
-Quickly get posts from Reddit in your terminal window or Slack app. The default is World News, but I'll update it soon, so you can change it to any subreddit. The Reddit posts Op runs on the CTO.ai Ops CLI.
+Quickly get posts from Reddit in your terminal window or Slack app. The default is World News, but you can change it to any subreddit by providing the sub name (no need to add "r/").
 
 ### Installation
 
+The Reddit posts Op runs on the CTO.ai Ops CLI.
 First install the Ops Platform CLI from CTO.ai: https://cto.ai
 
 ```shell
@@ -11,10 +12,18 @@ npm i @cto.ai/ops
 ```
 
 ```shell
-ops run @cawood/reddit
+ops run @cawood/reddit <subredditName>
+```
+
+Add the Op to your team and you can run
+```shell
+ops run reddit <subredditName>
 ```
 
 ### Sample Output
 
-🌍 "Climate change deniers robbing Australia of time to respond to impacts, Australian Science Minister warns"
-"https://www.abc.net.au/news/2020-01-15/cabinet-ministers-karen-andrews-climate-change-time-wasters/11868694"
+🌍 Climate change deniers robbing Australia of time to respond to impacts, Australian Science Minister warns
+https://www.abc.net.au/news/2020-01-15/cabinet-ministers-karen-andrews-climate-change-time-wasters/11868694
+
+🥋 Yesterday I got my Purple Belt
+https://www.reddit.com/r/bjj/comments/exv29b/yesterday_i_got_my_purple_beltch/
--- index.js
+++ index.js
@@ -1,40 +1,53 @@
 const { ux } = require('@cto.ai/sdk')
 const https = require('https');
 const main = async () => {
-    var count = 5;
-    var sub = 'worldnews';
-    var path = '/r/' + sub + '/hot.json?count=' + count;
-    const options = {
-      hostname: 'www.reddit.com',
-      port: 443,
-      path: path,
-      method: 'GET'
-    }
+  var prefix = '';
+  var sub = process.argv.slice(2);
+  if (sub == '' || sub == 'undefined') {
+    sub = 'worldnews';
+    prefix = '🌍 ';
+  }
 
-    const req = https.request(options, res => {
-      var body = '';
-      res.on('data', function (chunk) {
-        body = body + chunk;
-      });
-      res.on('end',async function(){
-        if (res.statusCode != 200) {
-          await ux.print("Reddit call failed with response code " + res.statusCode);
-        } else {
-          try {
-            for(i = 0; i < count; i++) {
-              await ux.print('🌍 ' + JSON.stringify(JSON.parse(body).data.children[i].data.title));
-              await ux.print(JSON.stringify(JSON.parse(body).data.children[i].data.url) + '\n');
-            }
-          } catch (err) {
-            await ux.print('Error: ' + err);
+  if (sub == 'bjj') {
+    prefix = '🥋 ';
+  } else if (sub == 'worldnews') {
+    prefix = '🌍 ';
+  }
+
+  var count = 5;
+  var path = '/r/' + sub + '/hot.json?count=' + count;
+  const options = {
+    hostname: 'www.reddit.com',
+    port: 443,
+    path: path,
+    method: 'GET'
+  }
+
+  const req = https.request(options, res => {
+    var body = '';
+    res.on('data', function (chunk) {
+      body = body + chunk;
+    });
+    res.on('end',async function(){
+      if (res.statusCode != 200) {
+        await ux.print("Reddit call failed with response code " + res.statusCode);
+      } else {
+        try {
+          for(i = 0; i < count; i++) {
+            // worldnews '🌍 '
+            await ux.print(prefix + JSON.stringify(JSON.parse(body).data.children[i].data.title).slice(1,-1));
+            await ux.print(JSON.stringify(JSON.parse(body).data.children[i].data.url).slice(1,-1) + '\n');
           }
+        } catch (err) {
+          await ux.print('Error: ' + err);
         }
-      });
-    })
+      }
+    });
+  })
 
-    req.on('error', async (error) => {
-      await ux.print(error)
-    })
-    req.end()
+  req.on('error', async (error) => {
+    await ux.print(error)
+  })
+  req.end()
 }
 main()
